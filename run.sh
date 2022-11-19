4ee48417d2903c709519c9973bfdc2cb9bce154a
ricardax <contact@axelricard.fr>
2022-11-19 18:32:48 +0100

    transform  input path in absolute path
    Could be done directly in rust in the future

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-target_dir=$1 
+target_dir=$(realpath $1)
 
 docker build . --tag "link_checker_image:latest" --quiet 
 docker run -it -v "$target_dir":"$target_dir" link_checker_image:latest "$target_dir"
